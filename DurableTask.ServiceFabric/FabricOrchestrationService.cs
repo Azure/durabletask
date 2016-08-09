d7f7dcbf07c85b6a7894bd4b5c7409afca3da5b9
Pradeep Kadubandi <pradeek@microsoft.com>
2016-08-09 15:08:40 -0700

    Dummy classes for the minimally required interfaces for a custom provider (no implementation yet).

---
+++ DurableTask.ServiceFabric/FabricOrchestrationService.cs
@@ -0,0 +1,134 @@
+﻿//  ----------------------------------------------------------------------------------
+//  Copyright Microsoft Corporation
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ----------------------------------------------------------------------------------
+
+namespace DurableTask.ServiceFabric
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class FabricOrchestrationService : IOrchestrationService
+    {
+        public Task StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(bool isForced)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(bool recreateInstanceStore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateIfNotExistsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(bool deleteInstanceStore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMaxMessageCountExceeded(int currentMessageCount, OrchestrationRuntimeState runtimeState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDelayInSecondsAfterOnProcessException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDelayInSecondsAfterOnFetchException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TaskOrchestrationDispatcherCount { get; }
+        public int MaxConcurrentTaskOrchestrationWorkItems { get; }
+        public Task RenewTaskOrchestrationWorkItemLockAsync(TaskOrchestrationWorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CompleteTaskOrchestrationWorkItemAsync(
+            TaskOrchestrationWorkItem workItem,
+            OrchestrationRuntimeState newOrchestrationRuntimeState,
+            IList<TaskMessage> outboundMessages,
+            IList<TaskMessage> orchestratorMessages,
+            IList<TaskMessage> timerMessages,
+            TaskMessage continuedAsNewMessage,
+            OrchestrationState orchestrationState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AbandonTaskOrchestrationWorkItemAsync(TaskOrchestrationWorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReleaseTaskOrchestrationWorkItemAsync(TaskOrchestrationWorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TaskActivityDispatcherCount { get; }
+        public int MaxConcurrentTaskActivityWorkItems { get; }
+        public Task<TaskActivityWorkItem> RenewTaskActivityWorkItemLockAsync(TaskActivityWorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CompleteTaskActivityWorkItemAsync(TaskActivityWorkItem workItem, TaskMessage responseMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AbandonTaskActivityWorkItemAsync(TaskActivityWorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskActivityWorkItem> LockNextTaskActivityWorkItem(TimeSpan receiveTimeout, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskOrchestrationWorkItem> LockNextTaskOrchestrationWorkItemAsync(TimeSpan receiveTimeout, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
