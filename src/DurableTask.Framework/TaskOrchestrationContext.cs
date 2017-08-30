0c283b13c92873e22b4a2e261f50980af4b4e81f
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2017-08-29 23:42:43 -0700

    Fix for the issue where issuing TerminateInstanceAsync twice from client before the orchestration is picked up by dispatcher leads to a blocked orchestration (the processing keeps abandoning the orchestration). (#113)
    Only the first terminate request is honored and subsequent terminates are ignored. (That makes the fix simple and hopefully not a problematic behavior from user's perspective)

--- src/DurableTask.Framework/TaskOrchestrationContext.cs
+++ src/DurableTask.Framework/TaskOrchestrationContext.cs
@@ -33,6 +33,7 @@ namespace DurableTask
         readonly IDictionary<int, OrchestratorAction> orchestratorActionsMap;
         readonly TaskScheduler taskScheduler;
         OrchestrationCompleteOrchestratorAction continueAsNew;
+        bool executionTerminated;
         int idCounter;
 
         public TaskOrchestrationContext(OrchestrationInstance orchestrationInstance, TaskScheduler taskScheduler)
@@ -375,7 +376,11 @@ namespace DurableTask
 
         public void HandleExecutionTerminatedEvent(ExecutionTerminatedEvent terminatedEvent)
         {
-            CompleteOrchestration(terminatedEvent.Input, null, OrchestrationStatus.Terminated);
+            if (!executionTerminated)
+            {
+                executionTerminated = true;
+                CompleteOrchestration(terminatedEvent.Input, null, OrchestrationStatus.Terminated);
+            }
         }
 
         public void CompleteOrchestration(string result)
