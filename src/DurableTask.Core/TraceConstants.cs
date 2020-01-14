8363e6d201e9752ee557d3d161427fedf73cecd0
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-01-13 19:48:43 -0800

    Add copyright headers.

--- src/DurableTask.Core/TraceConstants.cs
+++ src/DurableTask.Core/TraceConstants.cs
@@ -1,6 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//  ----------------------------------------------------------------------------------
+//  Copyright Microsoft Corporation
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ----------------------------------------------------------------------------------
 
 namespace DurableTask.Core
 {
@@ -28,6 +37,5 @@ namespace DurableTask.Core
         /// DependencyDefault is the Distributed Tracing message default name for Dependency.
         /// </summary>
         public const string DependencyDefault = "outbound";
-
     }
 }
