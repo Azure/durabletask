535784aaa43a62b60bf5f7fe1e138c0dfe645bed
David Justo <david.justo.1996@gmail.com>
2020-11-11 11:18:21 -0800

    moved corr-sample-test into an existing test project

---
+++ test/DurableTask.AzureStorage.Tests/Correlation/StringExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿//  ----------------------------------------------------------------------------------
+//  Copyright Microsoft Corporation
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ----------------------------------------------------------------------------------
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DurableTask.AzureStorage.Tests.Correlation
+{
+
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void TestParseTraceParent()
+        {
+            string traceparentString = "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01";
+            TraceParent traceparent = traceparentString.ToTraceParent();
+            Assert.AreEqual("00", traceparent.Version);
+            Assert.AreEqual("4bf92f3577b34da6a3ce929d0e0e4736", traceparent.TraceId);
+            Assert.AreEqual("00f067aa0ba902b7", traceparent.SpanId);
+            Assert.AreEqual("01", traceparent.TraceFlags);
+        }
+
+        [TestMethod]
+        public void TestParseTraceParentThrowsException()
+        {
+            string wrongTraceparentString = "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7";
+            Assert.ThrowsException<ArgumentException>(
+                () => { wrongTraceparentString.ToTraceParent(); });
+        }
+
+        [TestMethod]
+        public void TestParseTraceParenWithNull()
+        {
+            string someString = null;
+            TraceParent result = someString?.ToTraceParent();
+            Assert.IsNull(result);
+        }
+    }
+}
