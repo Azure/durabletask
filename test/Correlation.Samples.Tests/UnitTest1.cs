9bcc1039f4825d7753fb2b54897ac8cab3836964
David Justo <david.justo.1996@gmail.com>
2020-11-11 10:52:50 -0800

    removed old correlation.sample.test project, replaced it with new one, currently with dummy test for CI sanity check

---
+++ test/Correlation.Samples.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Correlation.Samples.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
