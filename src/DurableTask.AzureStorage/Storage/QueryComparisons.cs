4c72ef13941df55b896d44efd9a3f9478f1f5826
amdeel <52223332+amdeel@users.noreply.github.com>
2021-07-26 16:38:36 -0700

    TableEntity and EntityProperty changes

--- src/DurableTask.AzureStorage/Storage/QueryComparisons.cs
+++ src/DurableTask.AzureStorage/Storage/QueryComparisons.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace DurableTask.AzureStorage.Storage
 {
-    class QueryComparisons
+    enum QueryComparisons
     {
-        internal static object Equal;
-        internal static object LessThanOrEqual;
-        internal static object GreaterThanOrEqual;
-        internal static object LessThan;
+        Equal,
+        LessThanOrEqual,
+        GreaterThanOrEqual,
+        LessThan
     }
 }
