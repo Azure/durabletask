2627c0c491536bc3fa4633a278c0b96a13346347
Chris Gillum <cgillum@microsoft.com>
2022-03-29 17:24:44 -0700

    Apply type fix suggestions from code review
    Co-authored-by: David Justo <david.justo.1996@gmail.com>

--- src/DurableTask.Core/TaskContext.cs
+++ src/DurableTask.Core/TaskContext.cs
@@ -33,7 +33,7 @@ namespace DurableTask.Core
         public OrchestrationInstance OrchestrationInstance { get; private set; }
 
         /// <summary>
-        /// Gets or sets a value indicating how to propogate unhandled exception metadata.
+        /// Gets or sets a value indicating how to propagate unhandled exception metadata.
         /// </summary>
         internal ErrorPropagationMode ErrorPropagationMode { get; set; }
     }
