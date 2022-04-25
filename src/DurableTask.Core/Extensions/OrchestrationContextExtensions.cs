e5de9a8d0c9f25a59f9f0cc29950f9b4f981b2f7
Leonid Rozhnov <lrozhnov@iiko.ru>
2022-04-26 01:13:18 +0200

    Generic extensions to create orchestrations in a strongly-typed manner

---
+++ src/DurableTask.Core/Extensions/OrchestrationContextExtensions.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DurableTask.Core
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Generic extensions to create orchestration activities type-safely.
+    /// </summary>
+    public static class OrchestrationContextExtensions
+    {
+        /// <summary>
+        /// Schedule a TaskActivity by type.
+        /// </summary>
+        /// <typeparam name="TActivity">Type that devices from TaskActivity class</typeparam>
+        /// <typeparam name="TResult">Return Type of the TaskActivity.Execute method</typeparam>
+        /// <typeparam name="TInput">Parameter type of TaskActivity</typeparam>
+        /// <param name="orchestrationContext"></param>
+        /// <param name="parameters">Parameters for the TaskActivity.Execute method</param>
+        /// <returns>Task that represents the execution of the specified TaskActivity</returns>
+        public static Task<TResult> ScheduleTask<TActivity, TInput, TResult>(this OrchestrationContext orchestrationContext, TInput parameters)
+            where TActivity: TaskActivity<TInput, TResult>
+        {
+            return orchestrationContext.ScheduleTask<TResult>(typeof(TActivity), parameters);
+        }
+    }
+}
---
+++ src/DurableTask.Core/Extensions/TaskHubClientExtensions.cs
@@ -0,0 +1,78 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DurableTask.Core
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Generic extensions to create orchestrations type-safely.
+    /// </summary>
+    public static class TaskHubClientExtensions
+    {
+        /// <summary>
+        /// Create a new orchestration of the specified type with an automatically generated instance id
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration{string, string}</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="input">Input parameter to the specified TaskOrchestration</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceAsync<TOrchestration>(this TaskHubClient taskHubClient, string input)
+            where TOrchestration : TaskOrchestration<string, string>
+        {
+            return taskHubClient.CreateOrchestrationInstanceAsync(typeof(TOrchestration), input);
+        }
+
+        /// <summary>
+        /// Create a new orchestration of the specified type with an automatically generated instance id
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="input">Input parameter to the specified TaskOrchestration</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(this TaskHubClient taskHubClient, TInput input)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateOrchestrationInstanceAsync(typeof(TOrchestration), input);
+        }
+
+        /// <summary>
+        /// Create a new orchestration of the specified type with the specified instance id
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="instanceId">Instance id for the orchestration to be created, must be unique across the Task Hub</param>
+        /// <param name="input">Input parameter to the specified TaskOrchestration</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(this TaskHubClient taskHubClient, string instanceId, TInput input)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateOrchestrationInstanceAsync(typeof(TOrchestration), instanceId, input);
+        }
+
+        /// <summary>
+        /// Create a new orchestration of the specified type with the specified instance id
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="instanceId">Instance id for the orchestration to be created, must be unique across the Task Hub</param>
+        /// <param name="input">Input parameter to the specified TaskOrchestration</param>
+        /// <param name="dedupeStatuses">States of previous orchestration executions to be considered while de-duping new orchestrations on the client</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(this TaskHubClient taskHubClient,
+            string instanceId,
+            TInput input,
+            OrchestrationStatus[] dedupeStatuses)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateOrchestrationInstanceAsync(typeof(TOrchestration), instanceId, input, dedupeStatuses);
+        }
+    }
+}
