d2a6807ef0828f8244b6beca1ff328cd661ea853
Leonid Rozhnov <lrozhnov@iiko.ru>
2022-04-30 00:53:20 +0200

    A bit more of extensions to fully cover OrchestrationContext/TaskHubClient functionality with generic implementations

--- src/DurableTask.Core/Extensions/OrchestrationContextExtensions.cs
+++ src/DurableTask.Core/Extensions/OrchestrationContextExtensions.cs
@@ -21,9 +21,99 @@ namespace DurableTask.Core
         /// <param name="parameters">Parameters for the TaskActivity.Execute method</param>
         /// <returns>Task that represents the execution of the specified TaskActivity</returns>
         public static Task<TResult> ScheduleTask<TActivity, TInput, TResult>(this OrchestrationContext orchestrationContext, TInput parameters)
-            where TActivity: TaskActivity<TInput, TResult>
+            where TActivity : TaskActivity<TInput, TResult>
         {
             return orchestrationContext.ScheduleTask<TResult>(typeof(TActivity), parameters);
+        }
+        
+        /// <summary>
+        ///     Schedule a TaskActivity by type. Also retry on failure as per supplied policy.
+        /// </summary>
+        /// <typeparam name="TActivity">Type that devices from TaskActivity class</typeparam>
+        /// <typeparam name="TResult">Return Type of the TaskActivity.Execute method</typeparam>
+        /// <typeparam name="TInput">Parameter type of TaskActivity</typeparam>
+        /// <param name="orchestrationContext"></param>
+        /// <param name="retryOptions">Retry policy</param>
+        /// <param name="parameters">Parameters for the TaskActivity.Execute method</param>
+        /// <returns>Task that represents the execution of the specified TaskActivity</returns>
+        public static Task<TResult> ScheduleWithRetry<TActivity, TInput, TResult>(
+            this OrchestrationContext orchestrationContext,
+            RetryOptions retryOptions,
+            params object[] parameters)
+            where TActivity : TaskActivity<TInput, TResult>
+        {
+            return orchestrationContext.ScheduleWithRetry<TResult>(typeof(TActivity), retryOptions, parameters);
+        }
+
+        /// <summary>
+        ///     Create a sub-orchestration of the specified type. Also retry on failure as per supplied policy.
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="orchestrationContext"></param>
+        /// <param name="retryOptions">Retry policy</param>
+        /// <param name="input">Input for the TaskOrchestration.RunTask method</param>
+        /// <returns>Task that represents the execution of the specified sub-orchestration</returns>
+        public static Task<TResult> CreateSubOrchestrationInstanceWithRetry<TOrchestration, TInput, TResult>(
+            this OrchestrationContext orchestrationContext,
+            RetryOptions retryOptions,
+            TInput input)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return orchestrationContext.CreateSubOrchestrationInstanceWithRetry<TResult>(typeof(TOrchestration), retryOptions, input);
+        }
+
+        /// <summary>
+        ///     Create a sub-orchestration of the specified type. Also retry on failure as per supplied policy.
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="orchestrationContext"></param>
+        /// <param name="instanceId">Instance Id of the sub-orchestration</param>
+        /// <param name="retryOptions">Retry policy</param>
+        /// <param name="input">Input for the TaskOrchestration.RunTask method</param>
+        /// <returns>Task that represents the execution of the specified sub-orchestration</returns>
+        public static Task<TResult> CreateSubOrchestrationInstanceWithRetry<TOrchestration, TInput, TResult>(
+            this OrchestrationContext orchestrationContext,
+            string instanceId,
+            RetryOptions retryOptions,
+            TInput input)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return orchestrationContext.CreateSubOrchestrationInstanceWithRetry<TResult>(typeof(TOrchestration), instanceId, retryOptions, input);
+        }
+
+        /// <summary>
+        ///     Create a sub-orchestration of the specified type.
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="orchestrationContext"></param>
+        /// <param name="input">Input for the TaskOrchestration.RunTask method</param>
+        /// <returns>Task that represents the execution of the specified sub-orchestration</returns>
+        public static Task<TResult> CreateSubOrchestrationInstance<TOrchestration, TInput, TResult>(this OrchestrationContext orchestrationContext, TInput input)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return orchestrationContext.CreateSubOrchestrationInstance<TResult>(typeof(TOrchestration), input);
+        }
+
+        /// <summary>
+        ///     Create a sub-orchestration of the specified type with the specified instance id
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="orchestrationContext"></param>
+        /// <param name="instanceId">InstanceId of the sub-orchestration to create</param>
+        /// <param name="input">Input for the TaskOrchestration.RunTask method</param>
+        /// <returns>Task that represents the execution of the specified sub-orchestration</returns>
+        public static Task<TResult> CreateSubOrchestrationInstance<TOrchestration, TInput, TResult>(this OrchestrationContext orchestrationContext, string instanceId, TInput input)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return orchestrationContext.CreateSubOrchestrationInstance<TResult>(typeof(TOrchestration), instanceId, input);
         }
     }
 }
--- src/DurableTask.Core/Extensions/TaskHubClientExtensions.cs
+++ src/DurableTask.Core/Extensions/TaskHubClientExtensions.cs
@@ -4,6 +4,7 @@
 
 namespace DurableTask.Core
 {
+    using System;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -66,13 +67,127 @@ namespace DurableTask.Core
         /// <param name="input">Input parameter to the specified TaskOrchestration</param>
         /// <param name="dedupeStatuses">States of previous orchestration executions to be considered while de-duping new orchestrations on the client</param>
         /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
-        public static Task<OrchestrationInstance> CreateOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(this TaskHubClient taskHubClient,
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(
+            this TaskHubClient taskHubClient,
             string instanceId,
             TInput input,
             OrchestrationStatus[] dedupeStatuses)
             where TOrchestration : TaskOrchestration<TInput, TResult>
         {
             return taskHubClient.CreateOrchestrationInstanceAsync(typeof(TOrchestration), instanceId, input, dedupeStatuses);
+        }
+
+        /// <summary>
+        ///     Creates an orchestration instance, and raises an event for it, which eventually causes the OnEvent() method in the
+        ///     orchestration to fire.
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="orchestrationInput">Input parameter to the specified TaskOrchestration</param>
+        /// <param name="eventName">Name of the event</param>
+        /// <param name="eventData">Data for the event</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        /// 
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceWithRaisedEventAsync<TOrchestration, TInput, TResult>(
+            this TaskHubClient taskHubClient,
+            TInput orchestrationInput,
+            string eventName,
+            object eventData)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateOrchestrationInstanceWithRaisedEventAsync(typeof(TOrchestration), orchestrationInput, eventName, eventData);
+        }
+
+        /// <summary>
+        ///     Creates an orchestration instance, and raises an event for it, which eventually causes the OnEvent() method in the
+        ///     orchestration to fire.
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="instanceId">Instance id for the orchestration to be created, must be unique across the Task Hub</param>
+        /// <param name="orchestrationInput">Input parameter to the specified TaskOrchestration</param>
+        /// <param name="eventName">Name of the event</param>
+        /// <param name="eventData">Data for the event</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceWithRaisedEventAsync<TOrchestration, TInput, TResult>(
+            this TaskHubClient taskHubClient,
+            string instanceId,
+            TInput orchestrationInput,
+            string eventName,
+            object eventData)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateOrchestrationInstanceWithRaisedEventAsync(typeof(TOrchestration), instanceId, orchestrationInput, eventName, eventData);
+        }
+
+        /// <summary>
+        ///     Creates an orchestration instance, and raises an event for it, which eventually causes the OnEvent() method in the
+        ///     orchestration to fire.
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="instanceId">Instance id for the orchestration to be created, must be unique across the Task Hub</param>
+        /// <param name="orchestrationInput">Input parameter to the specified TaskOrchestration</param>
+        /// <param name="dedupeStatuses">States of previous orchestration executions to be considered while de-duping new orchestrations on the client</param>
+        /// <param name="eventName">Name of the event</param>
+        /// <param name="eventData">Data for the event</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateOrchestrationInstanceWithRaisedEventAsync<TOrchestration, TInput, TResult>(
+            this TaskHubClient taskHubClient,
+            string instanceId,
+            TInput orchestrationInput,
+            OrchestrationStatus[] dedupeStatuses,
+            string eventName,
+            object eventData)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateOrchestrationInstanceWithRaisedEventAsync(typeof(TOrchestration), instanceId, orchestrationInput, eventName, eventData);
+        }
+
+        /// <summary>
+        ///     Create a new orchestration of the specified type with the specified instance id, scheduled to start at an specific time
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="input">Input parameter to the specified TaskOrchestration</param>
+        /// <param name="startAt">Orchestration start time</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateScheduledOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(
+            this TaskHubClient taskHubClient,
+            TInput input,
+            DateTime startAt)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateScheduledOrchestrationInstanceAsync(typeof(TOrchestration), input, startAt);
+        }
+
+        /// <summary>
+        ///     Create a new orchestration of the specified type with the specified instance id, scheduled to start at an specific time
+        /// </summary>
+        /// <typeparam name="TOrchestration">Type that derives from TaskOrchestration</typeparam>
+        /// <typeparam name="TInput">TaskOrchestration parameter type</typeparam>
+        /// <typeparam name="TResult">TaskOrchestration result type</typeparam>
+        /// <param name="taskHubClient"></param>
+        /// <param name="instanceId">Instance id for the orchestration to be created, must be unique across the Task Hub</param>
+        /// <param name="input">Input parameter to the specified TaskOrchestration</param>
+        /// <param name="startAt">Orchestration start time</param>
+        /// <returns>OrchestrationInstance that represents the orchestration that was created</returns>
+        public static Task<OrchestrationInstance> CreateScheduledOrchestrationInstanceAsync<TOrchestration, TInput, TResult>(
+            this TaskHubClient taskHubClient,
+            string instanceId,
+            TInput input,
+            DateTime startAt)
+            where TOrchestration : TaskOrchestration<TInput, TResult>
+        {
+            return taskHubClient.CreateScheduledOrchestrationInstanceAsync(typeof(TOrchestration), instanceId, input, startAt);
         }
     }
 }
