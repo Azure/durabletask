374b9f25da5ac0778a28e6f61516c25e80d38631
Chris Gillum <cgillum@microsoft.com>
2022-09-06 18:04:03 -0700

    Add defense against corrupt history state

--- src/DurableTask.AzureStorage/OrchestrationSessionManager.cs
+++ src/DurableTask.AzureStorage/OrchestrationSessionManager.cs
@@ -549,7 +549,7 @@ namespace DurableTask.AzureStorage
 
                         return session;
                     }
-                    else if (nextBatch.OrchestrationExecutionId == existingSession.Instance.ExecutionId)
+                    else if (nextBatch.OrchestrationExecutionId == existingSession.Instance?.ExecutionId)
                     {
                         // there is already an active session with the same execution id.
                         // The session might be waiting for more messages. If it is, signal them.
