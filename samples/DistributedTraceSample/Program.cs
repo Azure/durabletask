0e0c77e893e78e46016e3a4b9a025a51515f4f8e
Varshitha Bachu <vabachu@microsoft.com>
2023-02-21 12:00:35 -0800

    Add README for Distributed Tracing with OpenTelemetry sample (#861)

--- samples/DistributedTraceSample/Program.cs
+++ samples/DistributedTraceSample/Program.cs
@@ -46,20 +46,10 @@ namespace OpenTelemetrySample
             // worker.ErrorPropagationMode = ErrorPropagationMode.SerializeExceptions;
             worker.ErrorPropagationMode = ErrorPropagationMode.UseFailureDetails;
 
-            worker.AddTaskOrchestrations(typeof(HelloSubOrch));
+            worker.AddTaskOrchestrations(typeof(Orchestration));
             worker.AddTaskOrchestrations(typeof(HelloSequence));
-            worker.AddTaskOrchestrations(typeof(HelloFanOut));
-            worker.AddTaskOrchestrations(typeof(HelloFanOutWithException));
-            worker.AddTaskOrchestrations(typeof(ExceptionSubOrch));
             worker.AddTaskOrchestrations(typeof(ExceptionOrchestration));
             worker.AddTaskActivities(typeof(SayHello));
-            worker.AddTaskActivities(typeof(GetRequestResultMessageActivity));
-            worker.AddTaskActivities(typeof(ExceptionActivity));
-            worker.AddTaskOrchestrations(typeof(GetRequestionDecisionOrchestration));
-            worker.AddTaskOrchestrations(typeof(EventConversationOrchestration));
-            worker.AddTaskOrchestrations(typeof(EventConversationOrchestration.Responder));
-            worker.AddTaskOrchestrations(typeof(HelloSequenceWithTimer));
-            worker.AddTaskOrchestrations(typeof(HelloSequenceException));
 
             await worker.StartAsync();
 
@@ -71,77 +61,10 @@ namespace OpenTelemetrySample
 
             TaskHubClient client = new TaskHubClient(serviceClient);
 
-            // Hello Sequence
-            OrchestrationInstance helloSeqInstance = await client.CreateOrchestrationInstanceAsync(
-                typeof(HelloSequence),
+            OrchestrationInstance orchestrationInstance = await client.CreateOrchestrationInstanceAsync(
+                typeof(Orchestration),
                 input: null);
-            await client.WaitForOrchestrationAsync(helloSeqInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Sequence!");
-
-            // Hello Sequence throws exception
-            OrchestrationInstance helloSeqExceptionInstance = await client.CreateOrchestrationInstanceAsync(
-                typeof(HelloSequenceException),
-                input: null);
-            await client.WaitForOrchestrationAsync(helloSeqExceptionInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Sequence Exception!");
-
-            // Hello Fan Out Fan In
-            OrchestrationInstance helloFanOutFanInInstance = await client.CreateOrchestrationInstanceAsync(
-                typeof(HelloFanOut),
-                input: null);
-            await client.WaitForOrchestrationAsync(helloFanOutFanInInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Fan Out Fan In!");
-
-            // Hello Fan Out Fan In with failed Activity
-            OrchestrationInstance helloFanOutFanInActivityExceptionInstance = await client.CreateOrchestrationInstanceAsync(
-                typeof(HelloFanOutWithException),
-                input: null);
-            await client.WaitForOrchestrationAsync(helloFanOutFanInActivityExceptionInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Fan Out Fan In with failed Activity!");
-
-            // Sub orchestrations
-            OrchestrationInstance helloSubOrchInstance = await client.CreateOrchestrationInstanceAsync(
-                typeof(HelloSubOrch),
-                input: null);
-            await client.WaitForOrchestrationAsync(helloSubOrchInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Sub-orchestration!");
-
-            // Sub orchestration failure
-            OrchestrationInstance helloSubOrchFailedInstance = await client.CreateOrchestrationInstanceAsync(
-                typeof(ExceptionSubOrch),
-                input: null);
-            await client.WaitForOrchestrationAsync(helloSubOrchFailedInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Sub-orchestration with failed sub-orchestration!");
-
-            // Hello Sequence with Timer
-            OrchestrationInstance helloSeqWithTimerInstance = await client.CreateOrchestrationInstanceAsync(typeof(HelloSequenceWithTimer), null);
-            await client.WaitForOrchestrationAsync(helloSeqWithTimerInstance, TimeSpan.FromMinutes(5));
-
-            Console.WriteLine("Done with Hello Sequence with Timer!");
-
-            // External event - SendEvent
-            OrchestrationInstance eventConversationInstance = await client.CreateOrchestrationInstanceAsync(typeof(EventConversationOrchestration), null);
-            OrchestrationState state = await client.WaitForOrchestrationAsync(eventConversationInstance, TimeSpan.FromMinutes(5));
-            
-            Console.WriteLine(state.Output);
-
-            Console.WriteLine("Done with raising an external event using OrchestrationContext!");
-
-            // External event - RaiseEvent
-            OrchestrationInstance getRequestDecisionInstance = await client.CreateOrchestrationInstanceAsync(typeof(GetRequestionDecisionOrchestration), null);
-            await client.RaiseEventAsync(getRequestDecisionInstance, "RequestDecision", "approved");
-            OrchestrationState firstInstanceState = await client.WaitForOrchestrationAsync(getRequestDecisionInstance, TimeSpan.FromMinutes(2));
-
-            Console.WriteLine("Result:");
-            Console.WriteLine(firstInstanceState.Output);
-
-            Console.WriteLine("Done with raising an external event with RaiseEventAsync!");
+            await client.WaitForOrchestrationAsync(orchestrationInstance, TimeSpan.FromMinutes(5));
         }
 
         static async Task<(IOrchestrationService, IOrchestrationServiceClient)> GetOrchestrationServiceAndClient()
@@ -158,31 +81,16 @@ namespace OpenTelemetrySample
             return (service, client);
         }
 
-        class HelloSubOrch : TaskOrchestration<string, string>
+        class Orchestration : TaskOrchestration<string, string>
         {
             public override async Task<string> RunTask(OrchestrationContext context, string input)
             {
                 string result = "";
                 result += await context.CreateSubOrchestrationInstance<string>(typeof(HelloSequence), null);
                 result += await context.ScheduleTask<string>(typeof(SayHello), "Tokyo");
-                Task<string> parallelTask = context.CreateSubOrchestrationInstance<string>(typeof(HelloFanOut), null);
+                Task<string> parallelTask = context.CreateSubOrchestrationInstance<string>(typeof(ExceptionOrchestration), "orchestration threw an exception");
                 result += await context.CreateSubOrchestrationInstance<string>(typeof(HelloSequence), null);
                 result += await parallelTask;
-
-                return result;
-            }
-        }
-
-        class ExceptionSubOrch : TaskOrchestration<string, string>
-        {
-            public override async Task<string> RunTask(OrchestrationContext context, string input)
-            {
-                string result = "";
-                result += await context.CreateSubOrchestrationInstance<string>(typeof(HelloSequence), null);
-                result += await context.ScheduleTask<string>(typeof(SayHello), "Tokyo");
-                Task<string> fanOut = context.CreateSubOrchestrationInstance<string>(typeof(ExceptionOrchestration), "activity threw an exception");
-                result += await context.CreateSubOrchestrationInstance<string>(typeof(HelloSequence), null);
-                result += await fanOut;
 
                 return result;
             }
@@ -208,204 +116,12 @@ namespace OpenTelemetrySample
             }
         }
 
-        class HelloFanOut : TaskOrchestration<string, string>
-        {
-            public override async Task<string> RunTask(OrchestrationContext context, string input)
-            {
-                string[] results = await Task.WhenAll(
-                    context.ScheduleTask<string>(typeof(SayHello), "Tokyo"),
-                    context.ScheduleTask<string>(typeof(SayHello), "London"),
-                    context.ScheduleTask<string>(typeof(SayHello), "Seattle"));
-                
-                return string.Join(", ", results);
-            }
-        }
-
-        class HelloFanOutWithException : TaskOrchestration<string, string>
-        {
-            public override async Task<string> RunTask(OrchestrationContext context, string input)
-            {
-                string[] results = await Task.WhenAll(
-                    context.ScheduleTask<string>(typeof(SayHello), "Tokyo"),
-                    context.ScheduleTask<string>(typeof(ExceptionActivity), ""),
-                    context.ScheduleTask<string>(typeof(SayHello), "Seattle"));
-
-                return string.Join(", ", results);
-            }
-        }
-
         class SayHello : TaskActivity<string, string>
         {
             protected override string Execute(TaskContext context, string input)
             {
                 Thread.Sleep(1000);
                 return $"Hello, {input}!";
-            }
-        }
-
-        class HelloSequenceException : TaskOrchestration<string, string>
-        {
-            public override async Task<string> RunTask(OrchestrationContext context, string input)
-            {
-                string output = "";
-                output += await context.ScheduleTask<string>(typeof(SayHello), "Tokyo") + ", ";
-                output += await context.ScheduleTask<string>(typeof(SayHello), "London") + ", ";
-                output += await context.ScheduleTask<string>(typeof(ExceptionActivity), "This is an invalid operation.") + ", ";
-                output += await context.ScheduleTask<string>(typeof(SayHello), "Seattle");
-                return output;
-            }
-        }
-        class ExceptionActivity : TaskActivity<string, string>
-        {
-            protected override string Execute(TaskContext context, string input)
-            {
-                throw new InvalidOperationException(input);
-            }
-        }
-
-        class HelloSequenceWithTimer : TaskOrchestration<string, string>
-        {
-            public override async Task<string> RunTask(OrchestrationContext context, string input)
-            {
-                string output = "";
-                output += await context.ScheduleTask<string>(typeof(SayHello), "Tokyo") + ", ";
-                await context.CreateTimer(context.CurrentUtcDateTime.AddSeconds(15), true);
-                output += await context.ScheduleTask<string>(typeof(SayHello), "London") + ", ";
-                output += await context.ScheduleTask<string>(typeof(SayHello), "Seattle");
-                return output;
-            }
-        }
-
-        class GetRequestResultMessageActivity : TaskActivity<string, string>
-        {
-            protected override string Execute(TaskContext context, string input)
-            {
-                switch (input)
-                {
-                    case "approved":
-                        return "Approved request!";
-                    case "declined":
-                        return "Declined request! Please submit another requst.";
-                    default:
-                        return "Unable to understand input. Please try again.";
-                }
-            }
-        }
-
-        public class GetRequestionDecisionOrchestration : TaskOrchestration<string, string>
-        {
-            TaskCompletionSource<object> getPermission = new TaskCompletionSource<object>();
-
-            public override async Task<string> RunTask(OrchestrationContext context, string input)
-            {
-                string decision = (string)await getPermission.Task;
-                string result = await context.ScheduleTask<string>(typeof(GetRequestResultMessageActivity), decision);
-                return result;
-            }
-
-            public override void OnEvent(OrchestrationContext context, string name, string input)
-            {
-                getPermission.SetResult(input);
-            }
-        }
-
-        public sealed class EventConversationOrchestration : TaskOrchestration<string, bool>
-        {
-            private readonly TaskCompletionSource<string> tcs1
-                = new TaskCompletionSource<string>(TaskContinuationOptions.ExecuteSynchronously);
-
-            // HACK: This is just a hack to communicate result of orchestration back to test
-            public static bool OkResult;
-
-            public async override Task<string> RunTask(OrchestrationContext context, bool useFireAndForgetSubOrchestration)
-            {
-                // start a responder orchestration
-                var responderId = "responderId";
-                Task<string> responderOrchestration = null;
-
-                if (!useFireAndForgetSubOrchestration)
-                {
-                    responderOrchestration = context.CreateSubOrchestrationInstance<string>(typeof(Responder), responderId, "Seattle");
-                }
-                else
-                {
-                    var dummyTask = context.CreateSubOrchestrationInstance<object>(NameVersionHelper.GetDefaultName(typeof(Responder)), "", responderId, "Seattle",
-                        new Dictionary<string, string>() { { OrchestrationTags.FireAndForget, "" } });
-
-                    if (!dummyTask.IsCompleted)
-                    {
-                        throw new Exception("test failed: fire-and-forget should complete immediately");
-                    }
-
-                    responderOrchestration = Task.FromResult("Bye from Seattle");
-                }
-
-                // send the id of this orchestration to the responder
-                var responderInstance = new OrchestrationInstance() { InstanceId = responderId };
-                context.SendEvent(responderInstance, channelName, context.OrchestrationInstance.InstanceId);
-
-                // wait for a response event 
-                var message = await tcs1.Task;
-                if (message != "Hi from Seattle")
-                    throw new Exception("test failed");
-
-                // tell the responder to stop listening
-                context.SendEvent(responderInstance, channelName, "stop");
-
-                // if this was not a fire-and-forget orchestration, wait for it to complete
-                var receiverResult = await responderOrchestration;
-
-                if (receiverResult != "Bye from Seattle")
-                    throw new Exception("test failed");
-
-                OkResult = true;
-
-                return "OK";
-            }
-
-            public override void OnEvent(OrchestrationContext context, string name, string input)
-            {
-                if (name == channelName)
-                {
-                    tcs1.TrySetResult(input);
-                }
-            }
-
-            private const string channelName = "conversation";
-
-            public class Responder : TaskOrchestration<string, string>
-            {
-                private readonly TaskCompletionSource<string> tcs2
-                    = new TaskCompletionSource<string>(TaskContinuationOptions.ExecuteSynchronously);
-
-                public async override Task<string> RunTask(OrchestrationContext context, string input)
-                {
-                    var message = await tcs2.Task;
-
-                    if (message == "stop")
-                    {
-                        return $"Bye from {input}";
-                    }
-                    else
-                    {
-                        // send a message back to the sender
-                        var senderInstance = new OrchestrationInstance() { InstanceId = message };
-                        context.SendEvent(senderInstance, channelName, $"Hi from {input}");
-
-                        // start over to wait for the next message
-                        context.ContinueAsNew(input);
-
-                        return "this value is meaningless";
-                    }
-                }
-
-                public override void OnEvent(OrchestrationContext context, string name, string input)
-                {
-                    if (name == channelName)
-                    {
-                        tcs2.TrySetResult(input);
-                    }
-                }
             }
         }
     }
