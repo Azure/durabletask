34e6ffef63ebb5fcffdf24deb03175199f1c988c
David Justo <david.justo.1996@gmail.com>
2023-03-21 17:44:44 -0700

    remove extranious import statement

--- src/DurableTask.AzureStorage/OrchestrationSessionManager.cs
+++ src/DurableTask.AzureStorage/OrchestrationSessionManager.cs
@@ -17,7 +17,6 @@ namespace DurableTask.AzureStorage
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.Remoting.Lifetime;
     using System.Threading;
     using System.Threading.Tasks;
     using DurableTask.AzureStorage.Messaging;
