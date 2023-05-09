66456dc5a9c7b9f975fa948aa9361658fb7bec6d
prajjawal05 <prajjawalagarwal@gmail.com>
2023-05-09 13:46:37 -0400

    Added logs in Queue cs

--- src/DurableTask.AzureStorage/Storage/Queue.cs
+++ src/DurableTask.AzureStorage/Storage/Queue.cs
@@ -18,6 +18,7 @@ namespace DurableTask.AzureStorage.Storage
     using System.Threading;
     using System.Threading.Tasks;
     using DurableTask.AzureStorage.Monitoring;
+    using DurableTask.AzureStorage.Logging;
     using Microsoft.WindowsAzure.Storage.Queue;
 
     class Queue
@@ -47,6 +48,7 @@ namespace DurableTask.AzureStorage.Storage
         {
             // Infinite time to live
             TimeSpan? timeToLive = TimeSpan.FromSeconds(-1);
+            DataFlowLogger.LogInformation(String.Format("[AddMessageAsync] [{0}] Message Length: {1}", this.Name, queueMessage.Message.Length));
 #if NET462
             // WindowsAzure.Storage 7.2.1 does not allow infinite time to live. Passing in null will default the time to live to 7 days.
             timeToLive = null;
@@ -66,6 +68,7 @@ namespace DurableTask.AzureStorage.Storage
 
         public async Task UpdateMessageAsync(QueueMessage queueMessage, TimeSpan visibilityTimeout, Guid? clientRequestId = null)
         {
+            DataFlowLogger.LogInformation(String.Format("[UpdateMessageAsync] [{0}] Message Length: {1}", this.Name, queueMessage.Message.Length));
             await this.azureStorageClient.MakeQueueStorageRequest(
                 (context, cancellationToken) => this.cloudQueue.UpdateMessageAsync(
                     queueMessage.CloudQueueMessage,
@@ -81,6 +84,7 @@ namespace DurableTask.AzureStorage.Storage
 
         public async Task DeleteMessageAsync(QueueMessage queueMessage, Guid? clientRequestId = null)
         {
+            DataFlowLogger.LogInformation(String.Format("[DeleteMessageAsync] [{0}]", this.Name));
             await this.azureStorageClient.MakeQueueStorageRequest(
                 (context, cancellationToken) => this.cloudQueue.DeleteMessageAsync(
                     queueMessage.CloudQueueMessage,
@@ -112,11 +116,14 @@ namespace DurableTask.AzureStorage.Storage
             }
 
             this.stats.MessagesRead.Increment();
-            return new QueueMessage(cloudQueueMessage);
+            var queueMessage = new QueueMessage(cloudQueueMessage);
+            DataFlowLogger.LogInformation(String.Format("[GetMessageAsync] [{0}] Message Length: {1}", this.Name, queueMessage.Message.Length));
+            return queueMessage;
         }
 
         public async Task<bool> ExistsAsync()
         {
+            DataFlowLogger.LogInformation(String.Format("[ExistsAsync] [{0}] ", this.Name));
             return await this.azureStorageClient.MakeQueueStorageRequest<bool>(
                 (context, cancellationToken) => this.cloudQueue.ExistsAsync(null, context, cancellationToken),
                 "Queue Exists");
@@ -124,6 +131,7 @@ namespace DurableTask.AzureStorage.Storage
 
         public async Task<bool> CreateIfNotExistsAsync()
         {
+            DataFlowLogger.LogInformation(String.Format("[CreateIfNotExistsAsync] [{0}] ", this.Name));
             return await this.azureStorageClient.MakeQueueStorageRequest<bool>(
                 (context, cancellationToken) => this.cloudQueue.CreateIfNotExistsAsync(null, context, cancellationToken),
                 "Queue Create");
@@ -131,6 +139,7 @@ namespace DurableTask.AzureStorage.Storage
 
         public async Task<bool> DeleteIfExistsAsync()
         {
+            DataFlowLogger.LogInformation(String.Format("[DeleteIfExistsAsync] [{0}] ", this.Name));
             return await this.azureStorageClient.MakeQueueStorageRequest<bool>(
                 (context, cancellationToken) => this.cloudQueue.DeleteIfExistsAsync(null, context, cancellationToken),
                 "Queue Delete");
@@ -156,7 +165,9 @@ namespace DurableTask.AzureStorage.Storage
             var queueMessages = new List<QueueMessage>();
             foreach (CloudQueueMessage cloudQueueMessage in cloudQueueMessages)
             {
-                queueMessages.Add(new QueueMessage(cloudQueueMessage));
+                var queueMessage = new QueueMessage(cloudQueueMessage);
+                DataFlowLogger.LogInformation(String.Format("[GetMessagesAsync] [{0}] Message Length: {1}", this.Name, queueMessage.Message.Length));
+                queueMessages.Add(queueMessage);
                 this.stats.MessagesRead.Increment();
             }
 
@@ -165,6 +176,7 @@ namespace DurableTask.AzureStorage.Storage
 
         public async Task FetchAttributesAsync()
         {
+            DataFlowLogger.LogInformation(String.Format("[FetchAttributesAsync] [{0}] ", this.Name));
             await this.azureStorageClient.MakeQueueStorageRequest(
                 (context, cancellationToken) => this.cloudQueue.FetchAttributesAsync(null, context, cancellationToken),
                 "Queue FetchAttributes");
@@ -179,7 +191,9 @@ namespace DurableTask.AzureStorage.Storage
             var queueMessages = new List<QueueMessage>();
             foreach (CloudQueueMessage cloudQueueMessage in cloudQueueMessages)
             {
-                queueMessages.Add(new QueueMessage(cloudQueueMessage));
+                var queueMessage = new QueueMessage(cloudQueueMessage);
+                DataFlowLogger.LogInformation(String.Format("[PeekMessagesAsync] [{0}] Message Length: {1}", this.Name, queueMessage.Message.Length));
+                queueMessages.Add(queueMessage);
                 this.stats.MessagesRead.Increment();
             }
 
@@ -188,8 +202,14 @@ namespace DurableTask.AzureStorage.Storage
 
         public async Task<QueueMessage?> PeekMessageAsync()
         {
-            var queueMessage = await this.cloudQueue.PeekMessageAsync();
-            return queueMessage == null ? null : new QueueMessage(queueMessage);
+            var cloudQueueMessage = await this.cloudQueue.PeekMessageAsync();
+            if (cloudQueueMessage == null)
+            {
+                return null;
+            }
+            var queueMessage = new QueueMessage(cloudQueueMessage);
+            DataFlowLogger.LogInformation(String.Format("[PeekMessageAsync] [{0}] Message Length: {1}", this.Name, queueMessage.Message.Length));
+            return queueMessage;
         }
     }
 }
