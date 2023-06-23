b2e36f6079c437d13cc64865e2443f3aa8f30fd8
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2023-06-23 14:32:21 -0700

    mark test disabledinci

--- Test/DurableTask.AzureStorage.Tests/TestTablePartitionManager.cs
+++ Test/DurableTask.AzureStorage.Tests/TestTablePartitionManager.cs
@@ -582,6 +582,7 @@ namespace DurableTask.AzureStorage.Tests
         /// Ensure that all instances could be process sucessfully.
         /// </summary>
         /// <returns></returns>
+        [TestCategory("DisabledInCI")]
         [TestMethod]
         public async Task EnsureOwnedQueueExclusive()
         {
