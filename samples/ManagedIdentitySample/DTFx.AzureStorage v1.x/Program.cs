8ddb4a6b334b472e5f68263f498524ce6c8c8975
Naiyuan Tian <110135109+nytian@users.noreply.github.com>
2024-06-26 15:48:55 -0700

    Add Token Credential Samples with DTFx.AzureStorage v1 and v2 (#1118)
    * initial commit
    
    * Update ReadMe.md
    
    * update by comments
    
    * remove "en-us" in the link
    
    * remove en-us in the links
    
    * update by comments
    
    * udpate token credential sample
    
    * update by comment
    
    * Update ConsoleApp.csproj
    
    * Update ConsoleApp.csproj
    
    * remove duplicate method

---
+++ samples/ManagedIdentitySample/DTFx.AzureStorage v1.x/Program.cs
@@ -0,0 +1,72 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using DurableTask.AzureStorage;
+using DurableTask.Core;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Create credential based on the configuration
+        var credential = new DefaultAzureCredential();
+        string[] scopes = new string[] { "https://storage.azure.com/.default" };  // Scope for Azure Storage
+
+        static Task<NewTokenAndFrequency> RenewTokenFuncAsync(object state, CancellationToken cancellationToken)
+        {
+            var credential = new DefaultAzureCredential();
+            var initialToken = credential.GetToken(new TokenRequestContext(new[] { "https://storage.azure.com/.default" }));
+            var expiresAfter = initialToken.ExpiresOn - DateTimeOffset.UtcNow - TimeSpan.FromMinutes(10);
+            return Task.FromResult(new NewTokenAndFrequency(initialToken.Token, expiresAfter));
+        }
+
+        // Get the token
+        var accessToken = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(scopes));
+
+        var service = new AzureStorageOrchestrationService(new AzureStorageOrchestrationServiceSettings
+        {
+            StorageAccountDetails = new StorageAccountDetails
+            {
+                AccountName = "YourStorageAccount",
+                EndpointSuffix = "core.windows.net",
+                StorageCredentials = new StorageCredentials(new Microsoft.WindowsAzure.Storage.Auth.TokenCredential(
+                    accessToken.Token,
+                    RenewTokenFuncAsync,
+                    null,
+                    TimeSpan.FromMinutes(5)))
+            }
+        });
+
+        var client = new TaskHubClient(service);
+        var worker = new TaskHubWorker(service);
+
+        worker.AddTaskOrchestrations(typeof(SampleOrchestration));
+        worker.AddTaskActivities(typeof(SampleActivity));
+
+        await worker.StartAsync();
+
+        var instance = await client.CreateOrchestrationInstanceAsync(typeof(SampleOrchestration), "World");
+
+        var result = await client.WaitForOrchestrationAsync(instance, TimeSpan.FromMinutes(1));
+
+        Console.WriteLine($"Orchestration result : {result.Output}");
+
+        await worker.StopAsync();
+    }
+}
+
+public class SampleOrchestration : TaskOrchestration<string, string>
+{
+    public override async Task<string> RunTask(OrchestrationContext context, string input)
+    {
+        return await context.ScheduleTask<string>(typeof(SampleActivity), input);
+    }
+}
+
+public class SampleActivity : TaskActivity<string, string>
+{
+    protected override string Execute(TaskContext context, string input)
+    {
+        return "Hello, " + input + "!";
+    }
+}
---
+++ samples/ManagedIdentitySample/DTFx.AzureStorage v2.x/Program.cs
@@ -0,0 +1,53 @@
+﻿using DurableTask.AzureStorage;
+using DurableTask.Core;
+using Azure.Identity;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var credential = new DefaultAzureCredential();
+        
+        // Pass the credential created to the StorageAccountClientProvider to start an AzureStorageOrchestrationService
+        var service = new AzureStorageOrchestrationService(new AzureStorageOrchestrationServiceSettings
+        {
+            StorageAccountClientProvider = new StorageAccountClientProvider("AccountName", credential),
+        });
+
+        var client = new TaskHubClient(service);
+        var worker = new TaskHubWorker(service);
+
+        worker.AddTaskOrchestrations(typeof(SampleOrchestration));
+        worker.AddTaskActivities(typeof(SampleActivity));
+
+        await worker.StartAsync();
+
+        var instance = await client.CreateOrchestrationInstanceAsync(typeof(SampleOrchestration), "World");
+
+        var result = await client.WaitForOrchestrationAsync(instance, TimeSpan.FromMinutes(1));
+
+        Console.WriteLine($"Orchestration result : {result.Output}");
+        
+        await worker.StopAsync();
+    }
+}
+
+public class SampleOrchestration : TaskOrchestration<string, string>
+{
+    public override async Task<string> RunTask(OrchestrationContext context, string input)
+    {
+        await context.ScheduleTask<string>(typeof(SampleActivity), input);
+
+        return "Orchestrator Finished!";
+    }
+}
+
+public class SampleActivity : TaskActivity<string, string>
+{
+    protected override string Execute(TaskContext context, string input)
+    {
+        Console.WriteLine("saying hello to " + input);
+        return "Hello " + input + "!";
+    }
+}
+
