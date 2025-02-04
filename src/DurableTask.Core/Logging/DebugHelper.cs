80a2ee90d4d37bff249953eb318e49dd4adb7020
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2025-02-04 10:50:22 -0800

    initial commit

---
+++ src/DurableTask.Core/Logging/DebugHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+#nullable enable
+namespace DurableTask.Core.Logging
+{
+    /// <summary>
+    /// Helper to write debug logs to trace at webjobs.worker.extensions level
+    /// </summary>
+    public class DebugHelper
+    {
+        readonly ILogger? log;
+
+        /// <summary>
+        /// Creates a new instance of the DebugHelper class
+        /// </summary>
+        public DebugHelper(ILogger? log)
+        {
+            // null is okay
+            this.log = log;
+        }
+
+        /// <summary>
+        /// Method to write OrchestrationDebugTrace logs to the ILogger. Debug uses only
+        /// </summary>
+        public void OrchestrationDebugTrace(string instanceId, string executionId, string details)
+        {
+            this.WriteStructuredLog(new LogEvents.OrchestrationDebugTrace(instanceId, executionId, details));
+        }
+
+        void WriteStructuredLog(ILogEvent logEvent, Exception? exception = null)
+        {
+            this.log?.LogDurableEvent(logEvent, exception);
+        }
+
+
+    }
+}
