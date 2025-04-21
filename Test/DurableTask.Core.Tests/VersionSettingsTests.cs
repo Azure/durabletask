fb66bcb214bb0e5cbc45ffd26f1f180fda85f4d5
halspang <halspang@microsoft.com>
2025-04-21 10:28:54 -0700

    Add copyright header
    Signed-off-by: halspang <halspang@microsoft.com>

--- Test/DurableTask.Core.Tests/VersionSettingsTests.cs
+++ Test/DurableTask.Core.Tests/VersionSettingsTests.cs
@@ -1,10 +1,18 @@
-﻿using DurableTask.Core.Settings;
+﻿//  ----------------------------------------------------------------------------------
+//  Copyright Microsoft Corporation
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ----------------------------------------------------------------------------------
+
+using DurableTask.Core.Settings;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DurableTask.Core.Tests
 {
