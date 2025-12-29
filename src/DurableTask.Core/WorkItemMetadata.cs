1ab9043dca83aaba542348abf33c419ed8a81957
Sophia Tevosyan <stevosyan@microsoft.com>
2025-12-29 15:36:52 -0800

    updating comments

--- src/DurableTask.Core/WorkItemMetadata.cs
+++ src/DurableTask.Core/WorkItemMetadata.cs
@@ -25,12 +25,12 @@ namespace DurableTask.Core
         }
 
         /// <summary>
-        /// Gets or sets whether or not the execution of the work item is within an extended session. 
+        /// Gets whether or not the execution of the work item is within an extended session. 
         /// </summary>
         public bool IsExtendedSession { get; private set; }
 
         /// <summary>
-        /// Gets or sets whether or not to include instance state when executing the work item via middleware.
+        /// Gets whether or not to include instance state when executing the work item via middleware.
         /// When false, this assumes that the middleware is able to handle extended sessions and has already cached
         /// the instance state from a previous execution, so it does not need to be included again.
         /// </summary>
